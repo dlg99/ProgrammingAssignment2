df8e503862f8793ef6d7cafb530d2c14f59b32ff
Andrey Yegorov <andrey.yegorov@gmail.com>
2014-09-20 23:49:37 -0700

    Assignment: Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions for creating and using inverted matrices which caching
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## Creates cacheable matrix
+makeCacheMatrix <- function(original = matrix()) {
+  
+  inverted <- NULL
+  
+  set <- function(y) {
+    original <<- y
+    inverted <<- NULL
+  }
+  
+  # getter and setter for inv. matrix value
+  get <- function() original
+  # Inversing the matrix using build in solve() function in R
+  set_inverted <- function(solve) inverted <<- solve
+  get_inverted <- function() inverted
+  
+  list(
+    set = set, 
+    get = get,
+    set_inverted = set_inverted,
+    get_inverted = get_inverted)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Inverses and caches matrix returned by makeCacheMatrix()
+## unless inversed matrix is already cached, then uses cached value
+## returns inversed matrix
+cacheSolve <- function(cacheable, ...) {
+  inverted <- cacheable$get_inverted()
+  # not cached?
+  if(is.null(inverted)) {
+    print("cache miss")
+    # invert and cache the matrix
+    original <- cacheable$get()
+    inverted <- solve(original)
+    cacheable$set_inverted(inverted)
+  } else {
+    print("cache hit")
+  }
+  # return inverted
+  inverted
 }
+
+test <- function() {
+  m <- makeCacheMatrix(matrix(1:4, nrow = 2, ncol = 2))
+  cacheSolve(m) # prints 'cache miss'
+  cacheSolve(m) # prints 'cache hit'
+}
+
+#test()
+
